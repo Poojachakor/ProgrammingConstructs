bca9afb7332342d66b666674da74fdaab6f9177f
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 17:59:53 +0530

    Add: UC7 file

--- ProgrammingConstructsProject/Program.cs
+++ ProgrammingConstructsProject/Program.cs
@@ -5,10 +5,54 @@
         static void Main(string[] args)
         {
 
-            WeekDaysName weekDaysName = new WeekDaysName();
-            weekDaysName.WeekDays();
+            Console.WriteLine("Enter the first number:");
+            double num1 = double.Parse(Console.ReadLine());
 
+            Console.WriteLine("Enter the second number:");
+            double num2 = double.Parse(Console.ReadLine());
 
+            Console.WriteLine("Choose an operation:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            switch (choice)
+            {
+                case 1:
+                    result = num1 + num2;
+                    Console.WriteLine("The result of addition is: " + result);
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    Console.WriteLine("The result of subtraction is: " + result);
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    Console.WriteLine("The result of multiplication is: " + result);
+                    break;
+                case 4:
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("The result of division is: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
         }
     }
+
+
 }
+    
