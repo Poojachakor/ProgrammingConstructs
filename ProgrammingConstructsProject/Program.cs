77b1c7d741584c8214472694edc67d266b57c080
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 18:06:22 +0530

    Add: UC8 file

--- ProgrammingConstructsProject/Program.cs
+++ ProgrammingConstructsProject/Program.cs
@@ -4,54 +4,20 @@
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a value for n: ");
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the first number:");
-            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Powers of 2 up to 2^{0}:", n);
 
-            Console.WriteLine("Enter the second number:");
-            double num2 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Choose an operation:");
-            Console.WriteLine("1. Addition");
-            Console.WriteLine("2. Subtraction");
-            Console.WriteLine("3. Multiplication");
-            Console.WriteLine("4. Division");
-
-            int choice = int.Parse(Console.ReadLine());
-
-            double result = 0;
-
-            switch (choice)
+            for (int i = 0; i <= n; i++)
             {
-                case 1:
-                    result = num1 + num2;
-                    Console.WriteLine("The result of addition is: " + result);
-                    break;
-                case 2:
-                    result = num1 - num2;
-                    Console.WriteLine("The result of subtraction is: " + result);
-                    break;
-                case 3:
-                    result = num1 * num2;
-                    Console.WriteLine("The result of multiplication is: " + result);
-                    break;
-                case 4:
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                        Console.WriteLine("The result of division is: " + result);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error: Division by zero is not allowed.");
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice.");
-                    break;
+                Console.WriteLine("2^{0} = {1}", i, Math.Pow(2, i));
             }
+
         }
+
     }
+
 
 
 }
