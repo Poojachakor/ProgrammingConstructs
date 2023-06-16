5994f188b2e3c4f9e917ebc0d778ea844b2ffd20
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 17:42:26 +0530

    Add: UC4 file

--- ProgrammingConstructsProject/Program.cs
+++ ProgrammingConstructsProject/Program.cs
@@ -4,19 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter three numbers:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int number3 = Convert.ToInt32(Console.ReadLine());
 
-            if (age >= 18)
-            {
-                Console.WriteLine("You are eligible to cast your vote.");
-            }
-            else
-            {
-                Console.WriteLine("You are not eligible to cast your vote yet.");
-            }
+            int largestNumber = GetLargestNumber(number1, number2, number3);
+            Console.WriteLine("The largest number is: " + largestNumber);
         }
 
+        static int GetLargestNumber(int num1, int num2, int num3)
+        {
+            int largest = num1;
+
+            if (num2 > largest)
+            {
+                largest = num2;
+            }
+
+            if (num3 > largest)
+            {
+                largest = num3;
+            }
+
+            return largest;
+        }
     }
+
+    
     
 }
