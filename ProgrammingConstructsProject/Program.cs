82922a6b4b386fa4881122f05617ed52a963fc1f
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 17:53:40 +0530

    Add: UC6 file

--- ProgrammingConstructsProject/Program.cs
+++ ProgrammingConstructsProject/Program.cs
@@ -5,28 +5,10 @@
         static void Main(string[] args)
         {
 
-            Console.Write("Input the marks obtained in Physics: ");
-            int physicsMarks = Convert.ToInt32(Console.ReadLine());
+            WeekDaysName weekDaysName = new WeekDaysName();
+            weekDaysName.WeekDays();
 
-            Console.Write("Input the marks obtained in Chemistry: ");
-            int chemistryMarks = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Input the marks obtained in Mathematics: ");
-            int mathMarks = Convert.ToInt32(Console.ReadLine());
-
-            if (mathMarks >= 65 && physicsMarks >= 55 && chemistryMarks >= 50 && (mathMarks + physicsMarks + chemistryMarks) >= 180)
-            {
-                Console.WriteLine("The candidate is eligible for admission.");
-            }
-            else if (mathMarks >= 65 && (physicsMarks + chemistryMarks) >= 140)
-            {
-                Console.WriteLine("The candidate is eligible for admission.");
-            }
-            else
-            {
-                Console.WriteLine("The candidate is not eligible for admission.");
-            }
         }
     }
-
 }
---
+++ ProgrammingConstructsProject/WeekDaysName.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructsProject
+{
+    internal class WeekDaysName
+    {
+        public void WeekDays()
+        {
+
+            Console.Write("Enter a weekday number from 1 to 7 ");
+            int weekdayNumber = Convert.ToInt32(Console.ReadLine());
+
+            string weekdayName = GetWeekdayName(weekdayNumber);
+            Console.WriteLine("The weekday name is: " + weekdayName);
+        }
+        public static string GetWeekdayName(int weekdayNumber)
+        {
+            string weekdayName;
+
+            switch (weekdayNumber)
+            {
+                case 1:
+                    weekdayName = "Sunday";
+                    break;
+                case 2:
+                    weekdayName = "Monday";
+                    break;
+                case 3:
+                    weekdayName = "Tuesday";
+                    break;
+                case 4:
+                    weekdayName = "Wednesday";
+                    break;
+                case 5:
+                    weekdayName = "Thursday";
+                    break;
+                case 6:
+                    weekdayName = "Friday";
+                    break;
+                case 7:
+                    weekdayName = "Saturday";
+                    break;
+                default:
+                    weekdayName = "Invalid weekday number";
+                    break;
+            }
+
+            return weekdayName;
+
+        }
+
+    }
+}
+
