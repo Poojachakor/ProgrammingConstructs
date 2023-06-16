0592b182d28d81d4b8536d7180205aef60dc70d1
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 17:24:06 +0530

    Add: ProgrammingConstructsProject

---
+++ ProgrammingConstructsProject/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ProgrammingConstructsProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second integer: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("The two integers are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The two integers are not equal.");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+    
+}
